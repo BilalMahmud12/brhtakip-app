b6d61a6b61448f2119d22a6ff531504f4770ab6d
Obad <apoeljzzar@gmail.com>
2024-05-29 17:25:01 +0300

    Added BrandStore to RootStore

--- src/stores/RootStore.ts
+++ src/stores/RootStore.ts
@@ -1,6 +1,7 @@
 import { makeObservable, extendObservable, observable, action } from 'mobx';
 import { UserStore } from './UserStore';
 import { RequestStore } from './RequestStore';
+import { BrandStore } from './BrandStore';
 import { ClientProfileStore } from './ClientProfileStore';
 import type { ClientProfile } from '@/API';
 
@@ -31,6 +32,7 @@ export class RootStore {
         this.userStore = new UserStore(this);
         this.requestStore = new RequestStore(this);
         this.clientProfileStore = new ClientProfileStore(this);
+        this.brandStore = new BrandStore(this);
 
         makeObservable(this, {
             setCurrentClientProfile: action,
