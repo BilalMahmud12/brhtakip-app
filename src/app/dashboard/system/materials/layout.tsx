a07f5da535d4573865ac834b56606a7bcd22e422
Abdelrahman <apoeljzzar@gmail.com>
2024-06-27 17:10:06 +0300

    Create Material Layout

---
+++ src/app/dashboard/system/materials/layout.tsx
@@ -0,0 +1,32 @@
+'use client'
+import React, { useEffect } from 'react'
+import * as Repo from '@/repository/index'
+import type { Material } from '@/API';
+import { useAppDispatch } from '@/reduxStore/hooks';
+import { AppDispatch } from '@/reduxStore/store';
+import { setMaterials } from '@/reduxStore/features/materialSlice'
+
+export default function MaterialLayout(
+    { children }: { children: React.ReactNode }
+) {
+
+    const dispatch = useAppDispatch<AppDispatch>();
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const materialData = await Repo.MaterialRepository.getAllMaterials();
+                dispatch(setMaterials(materialData as unknown as Material[]));
+                console.log('material data', materialData);
+            } catch (error) {
+                console.error('Failed to fetch materials', error);
+            }
+        }
+        fetchData();
+    });
+
+    return (
+        <div>
+            {children}
+        </div>
+    );
+}
